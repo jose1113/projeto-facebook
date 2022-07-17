07cd370e9f94059a450c3ff13eabef284958b73d
jose1113 <joseigor113@gmail.com>
2022-07-17 09:26:53 -0300

    Nome adicionado

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Projeto Facebook',
+    title: 'Facebook - entre ou cadastre-se',
     htmlAttrs: {
       lang: 'pt-br'
     },
@@ -13,7 +13,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/facebook_logo.ico' }
     ]
   },
 
